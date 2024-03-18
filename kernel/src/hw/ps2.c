eb8d15849eb939b660a91dde2f8b3a24a62cf7c1
Lexi Reyer <60185691+truemaker@users.noreply.github.com>
2024-03-18 19:19:22 +0100

    Added color to warnings and errors in PS/2

--- kernel/src/hw/ps2.c
+++ kernel/src/hw/ps2.c
@@ -97,7 +97,8 @@ void ps2_init() {
     asm("nop");
   if (ps2_read_data()) {
     kprint(
-        "WARNING: First PS/2 port failed, keyboard may not be available.\n\r");
+        TERMCODE(TC_FG_YELLOW) "WARNING: First PS/2 port failed, keyboard may "
+                               "not be available.\n\r" TERMCODE(TC_RESET));
   } else {
     port1_available = true;
   }
@@ -107,14 +108,16 @@ void ps2_init() {
       asm("nop");
     if (ps2_read_data()) {
       kprint(
-          "WARNING: Second PS/2 port failed, mouse may not be available.\n\r");
+          TERMCODE(TC_FG_YELLOW) "WARNING: Second PS/2 port failed, mouse may "
+                                 "not be available.\n\r" TERMCODE(TC_RESET));
     } else {
       port2_available = true;
     }
   }
   if (!(port1_available || port2_available)) {
-    kprint("ERROR: All PS/2 ports failed, keyboard and mouse will not be "
-           "available using PS/2.\n\r");
+    kprint(TERMCODE(TC_FG_RED) TERMCODE(
+        TC_BOLD) "ERROR: All PS/2 ports failed, keyboard and mouse will not be "
+                 "available using PS/2.\n\r" TERMCODE(TC_RESET));
     return;
   }
 }
