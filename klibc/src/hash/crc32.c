d2304d9e081cbd2272d27f541a638946fba33393
Lexi Reyer <60185691+truemaker@users.noreply.github.com>
2024-05-10 13:20:28 +0200

    take a guess

--- klibc/src/hash/crc32.c
+++ klibc/src/hash/crc32.c
@@ -3,7 +3,7 @@
 #include <stdint.h>
 #include <types.h>
 
-static u32 crc32_poly(u8 i) {
+static u32 _crc32_poly(u8 i) {
   u32 r = (u32)i;
   for (i32 j = 0; j < 8; j++) {
     bool l = r & 1;
@@ -17,6 +17,6 @@ static u32 crc32_poly(u8 i) {
 u32 crc32(u8 *p, u64 count) {
   u32 r = 0xffffffff;
   while (count-- != 0)
-    r = crc32_poly(((u8)r ^ *(p++))) ^ (r >> 8);
+    r = _crc32_poly(((u8)r ^ *(p++))) ^ (r >> 8);
   return ~r;
 }
