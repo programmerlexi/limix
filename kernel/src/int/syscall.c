355de5c1aa129d3bdc3d6a7c46a9e8d711150cd6
Lexi Reyer <60185691+truemaker@users.noreply.github.com>
2024-06-10 10:03:46 +0200

    Extended test syscall

--- kernel/src/int/syscall.c
+++ kernel/src/int/syscall.c
@@ -5,19 +5,32 @@
 #include "kernel/panic.h"
 #include "kernel/task/sched/global.h"
 #include "kernel/task/sched/local.h"
+#include "libk/ipc/spinlock.h"
 
 #undef DEBUG_MODULE
 #define DEBUG_MODULE "syscall"
 
 void syscall(int_frame_t *f) {
   u64 cpu = (u64)get_processor();
+  local_scheduler_t *ls = sched_get_local(cpu);
   switch (f->rax) {
   case SYSCALL_TEST:
+    static u32 lck = 0;
+    spinlock(&lck);
     log(LOGLEVEL_INFO, "Syscall test");
     logf(LOGLEVEL_INFO, "CPU: %u", cpu);
+    if (ls) {
+      log(LOGLEVEL_INFO, "Local scheduler is initialized");
+      if (ls->frames) {
+        logf(LOGLEVEL_INFO, "PID: %u TID: %u", ls->frames->frame->proc->pid,
+             ls->frames->frame->thread->tid);
+        logf(LOGLEVEL_INFO, "Process name: %s",
+             ls->frames->frame->proc->name.cstr);
+      }
+    }
+    spinunlock(&lck);
     break;
   case SYSCALL_YIELD:
-    local_scheduler_t *ls = sched_get_local(cpu);
     if (!ls) {
       log(LOGLEVEL_FATAL, "Received yield from non-existent cpu");
       kpanic();
