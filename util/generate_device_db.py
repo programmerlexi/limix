9a3a410d1a4d1b70824d8498f717579a1eaf1919
Lexi Reyer <60185691+truemaker@users.noreply.github.com>
2024-06-10 11:07:03 +0200

    Generate PCI device names from local pci.ids file provided by hwdata

---
+++ util/generate_device_db.py
@@ -0,0 +1,23 @@
+import sys
+
+with open(sys.argv[1], 'r') as f:
+    lines = f.read().splitlines()
+
+vID = ""
+
+for line in lines:
+    if line.strip().startswith('#'):
+        continue
+    if line.strip().startswith('C'):
+        break
+    if line.startswith("\t") and not line.startswith("\t"*2):
+        dID = line.strip().split(" ")[0]
+        dN = line.strip().split(" ")[2]
+        print("device_" + vID + dID + ":")
+        print(".start:")
+        print("dw 0x" + vID + ", 0x" + dID)
+        print("dw .end-.start")
+        print("db \""+dN+"\"")
+        print(".end:")
+    elif not line.startswith("\t"*2):
+        vID = line.strip().split(" ")[0]
