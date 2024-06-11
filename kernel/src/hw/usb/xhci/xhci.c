8a11c315dcc175281bcbb276ad58616888eb1be0
Lexi Reyer <60185691+truemaker@users.noreply.github.com>
2024-06-11 12:21:37 +0200

    Fix

--- kernel/src/hw/usb/xhci/xhci.c
+++ kernel/src/hw/usb/xhci/xhci.c
@@ -3,6 +3,7 @@
 #include "kernel/hw/pci/pci.h"
 #include "kernel/mm/heap.h"
 #include "kernel/mm/hhtp.h"
+#include <stddef.h>
 
 #undef DEBUG_MODULE
 #define DEBUG_MODULE "xhci"
@@ -10,9 +11,15 @@
 xhci_t *xhci_init(pci_type0_t *h) {
   xhci_t *xhci = kmalloc(sizeof(*xhci));
   xhci->xhci_device = h;
+  logf(LOGLEVEL_DEBUG, "%l %l", (u64)h->bar0, (u64)h->bar1);
   uptr phy_addr = h->bar0 & PCI_BAR_MEM_BASE_ADDR;
-  if ((h->bar0 & PCI_BAR_MEM_TYPE) == 2)
+  if ((h->bar0 & PCI_BAR_MEM_TYPE) >> 1 == 2)
     phy_addr |= (u64)(h->bar1 & PCI_BAR_MEM_BASE_ADDR) << 32;
+  if (!phy_addr) {
+    kfree(xhci);
+    log(LOGLEVEL_WARN2, "XHCI driver received nullptr");
+    return NULL;
+  }
   xhci->cap = (void *)HHDM(phy_addr);
   logf(LOGLEVEL_INFO, "Initialized XHCI %l %u", xhci->cap,
        (u64)xhci->cap->cap_length);
