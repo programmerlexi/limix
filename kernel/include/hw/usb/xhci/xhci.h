8e62c75335685aed8d580a095feb341af4593cde
Lexi Reyer <60185691+truemaker@users.noreply.github.com>
2024-06-11 11:11:39 +0200

    *Screeching*

--- kernel/include/hw/usb/xhci/xhci.h
+++ kernel/include/hw/usb/xhci/xhci.h
@@ -3,7 +3,21 @@
 #include "kernel/hw/pci/pci.h"
 
 typedef struct {
+  u8 cap_length;
+  u8 reserved0;
+  u16 interface_version;
+  u32 structural_parameters0;
+  u32 structural_parameters1;
+  u32 structural_parameters2;
+  u32 cap_params0;
+  u32 doorbell_offset;
+  u32 runtime_register_space_offset;
+  u32 cap_params1;
+} xhci_capability_register_t;
+
+typedef struct {
   pci_type0_t *xhci_device;
+  xhci_capability_register_t *cap;
 } xhci_t;
 
 xhci_t *xhci_init(pci_type0_t *h);
--- kernel/src/hw/usb/xhci/xhci.c
+++ kernel/src/hw/usb/xhci/xhci.c
@@ -2,6 +2,7 @@
 #include "kernel/debug.h"
 #include "kernel/hw/pci/pci.h"
 #include "kernel/mm/heap.h"
+#include "kernel/mm/hhtp.h"
 
 #undef DEBUG_MODULE
 #define DEBUG_MODULE "xhci"
@@ -9,6 +10,10 @@
 xhci_t *xhci_init(pci_type0_t *h) {
   xhci_t *xhci = kmalloc(sizeof(*xhci));
   xhci->xhci_device = h;
+  uptr phy_addr = h->bar0 & PCI_BAR_MEM_BASE_ADDR;
+  if ((h->bar0 & PCI_BAR_MEM_TYPE) == 2)
+    phy_addr |= (u64)(h->bar1 & PCI_BAR_MEM_BASE_ADDR) << 32;
+  xhci->cap = (void *)HHDM(phy_addr);
   log(LOGLEVEL_INFO, "Initialized XHCI");
   return xhci;
 }
