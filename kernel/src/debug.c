f7ea18ee6c50ba0abe7086b2f0a9b6f368275ff8
Lexi Reyer <60185691+truemaker@users.noreply.github.com>
2024-06-16 22:10:23 +0200

    log lock

--- kernel/src/debug.c
+++ kernel/src/debug.c
@@ -2,6 +2,7 @@
 #include "kernel/config.h"
 #include "kernel/gfx/vt/vt.h"
 #include "kernel/io/serial/serial.h"
+#include "libk/ipc/spinlock.h"
 #include "libk/printing.h"
 #include <stdarg.h>
 
@@ -20,9 +21,12 @@ static char *_logcolors[] = {TERMCODE(TC_FG_BLACK) TERMCODE(TC_BOLD),
                              TERMCODE(TC_FG_RED) TERMCODE(TC_BOLD),
                              TERMCODE(TC_FG_WHITE) TERMCODE(TC_DIM)};
 
+static u32 lock;
+
 void set_loglevel(loglevel_t ll) { _current_loglevel = ll; }
 void set_serial_loglevel(loglevel_t ll) { _current_serial_loglevel = ll; }
 void _log(loglevel_t ll, char *s) {
+  spinlock(&lock);
   if (ll >= _current_serial_loglevel) {
     serial_writes(s);
   }
@@ -30,8 +34,10 @@ void _log(loglevel_t ll, char *s) {
     kprint(_logcolors[ll]);
     kprint(s);
   }
+  spinunlock(&lock);
 }
 void _logf(loglevel_t ll, char *s, ...) {
+  spinlock(&lock);
   if (ll >= _current_loglevel) {
     kprint(_logcolors[ll]);
     va_list a;
@@ -39,4 +45,5 @@ void _logf(loglevel_t ll, char *s, ...) {
     kvprintf(s, a);
     va_end(a);
   }
+  spinunlock(&lock);
 }
