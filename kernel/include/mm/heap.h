fb7c5c61067b6694c5726a8e3426ed3e806d3116
programmerlexi <60185691+truemaker@users.noreply.github.com>
2024-07-20 14:59:55 +0200

    Add simple buffer overflow/underflow detection

--- kernel/include/mm/heap.h
+++ kernel/include/mm/heap.h
@@ -3,13 +3,17 @@
 #include "libk/types.h"
 
 #define HEAP_BASE 0xfffffff800000000
+#define HEAP_CANARY0 0x6865617062697264
+#define HEAP_CANARY1 0x6269726468656170
 
 typedef struct heapseg {
+  u64 canary0;
   usz size;
   struct heapseg *prev;
   struct heapseg *next;
   u64 used;
-} heapseg_t;
+  u64 canary1;
+} __attribute__((packed)) heapseg_t;
 
 void heap_init();
 u32 heap_get_used();
--- kernel/src/mm/heap.c
+++ kernel/src/mm/heap.c
@@ -1,5 +1,7 @@
 #include "kernel/mm/heap.h"
 #include "kernel/config.h"
+#include "kernel/debug.h"
+#include "kernel/kernel.h"
 #include "kernel/mm/hhtp.h"
 #include "kernel/mm/mm.h"
 #include "kernel/mm/vmm.h"
@@ -17,6 +19,14 @@ static u32 _heap_lock;
 
 static uptr heap_top;
 
+static void _heap_check_canary(heapseg_t *seg) {
+  nullsafe(seg);
+  if (seg->canary0 != HEAP_CANARY0)
+    kernel_panic_error("Heap canary0 invalid");
+  if (seg->canary1 != HEAP_CANARY1)
+    kernel_panic_error("Heap canary1 invalid");
+}
+
 void heap_init() {
   uptr heap_space = PHY(request_page_block(CONFIG_HEAP_INITIAL_PAGES));
   for (int i = 0; i < CONFIG_HEAP_INITIAL_PAGES; i++)
@@ -30,11 +40,16 @@ void heap_init() {
   _heap_first->prev = NULL;
   _heap_first->size = (CONFIG_HEAP_INITIAL_PAGES * 0x1000) - sizeof(heapseg_t);
   _heap_first->used = false;
+  _heap_first->canary0 = HEAP_CANARY0;
+  _heap_first->canary1 = HEAP_CANARY1;
   heap_top = HEAP_BASE + (CONFIG_HEAP_INITIAL_PAGES << 12);
 }
 
 static void _heap_combine_forward(heapseg_t *seg) {
+  nullsafe(seg);
+  _heap_check_canary(seg);
   nullsafe(seg->next);
+  _heap_check_canary(seg->next);
   if (seg->next->used)
     return;
   if ((uintptr_t)(seg->next) !=
@@ -49,12 +64,18 @@ static void _heap_combine_forward(heapseg_t *seg) {
 }
 
 static void _heap_combine_backward(heapseg_t *seg) {
-  if (seg->prev != NULL)
-    if (!seg->prev->used)
-      _heap_combine_forward(seg->prev);
+  nullsafe(seg);
+  _heap_check_canary(seg);
+  nullsafe(seg->prev);
+  _heap_check_canary(seg->prev);
+  if (seg->prev->used)
+    return;
+  _heap_combine_forward(seg->prev);
 }
 
 void expand_heap(usz size) {
+  if (size == 0)
+    return;
   if (size % 0x1000 > 0) {
     size -= size % 0x1000;
     size += 0x1000;
@@ -69,6 +90,8 @@ void expand_heap(usz size) {
       (heapseg_t *)((uptr)_heap_last + sizeof(*_heap_last) + _heap_last->size);
   new_seg->used = false;
   new_seg->prev = _heap_last;
+  new_seg->canary0 = HEAP_CANARY0;
+  new_seg->canary1 = HEAP_CANARY1;
   _heap_last->next = new_seg;
   _heap_last = new_seg;
   new_seg->next = NULL;
@@ -77,12 +100,16 @@ void expand_heap(usz size) {
 }
 
 static heapseg_t *_heap_split(heapseg_t *seg, usz size) {
+  nullsafe_error(seg, "Heap split with no segment");
+  _heap_check_canary(seg);
   if (size < 0x10)
     return NULL;
   usz splitSize = seg->size - size - sizeof(heapseg_t);
   if (splitSize < 0x10)
     return NULL;
   heapseg_t *new_seg = (heapseg_t *)((usz)seg + size + sizeof(heapseg_t));
+  new_seg->canary0 = HEAP_CANARY0;
+  new_seg->canary1 = HEAP_CANARY1;
 
   if (seg->next)
     seg->next->prev = new_seg;
@@ -101,6 +128,9 @@ void kfree(void *addr) {
   nullsafe(addr);
   spinlock(&_heap_lock);
   heapseg_t *seg = (heapseg_t *)addr - 1;
+  _heap_check_canary(seg);
+  if (!seg->used)
+    log(LOGLEVEL_WARN2, "Encountered double free");
   seg->used = false;
   _heap_combine_forward(seg);
   _heap_combine_backward(seg);
@@ -117,8 +147,11 @@ void *kmalloc(usz size) {
   spinlock(&_heap_lock);
   heapseg_t *seg = _heap_first;
   while (true) {
+    if (!seg)
+      kernel_panic_error("Heap segment missing");
+    _heap_check_canary(seg);
     if (!seg->used) {
-      if (seg->size > (size + sizeof(heapseg_t))) {
+      if (seg->size > (size + sizeof(heapseg_t) * 2)) {
         _heap_split(seg, size);
         seg->used = true;
         spinunlock(&_heap_lock);
@@ -149,6 +182,7 @@ u32 heap_get_used() {
   u32 used = 0;
   heapseg_t *c = _heap_first;
   while (c) {
+    _heap_check_canary(c);
     used += sizeof(heapseg_t);
     if (c->used)
       used += c->size;
