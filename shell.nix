abcb74f5e5b0968065e82ae6a44a50b02d0f209e
programmerlexi <60185691+truemaker@users.noreply.github.com>
2024-08-04 16:27:37 +0200

    Add c3 for future purposes

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
     name = "limix";
-    packages = with pkgs; [ gcc binutils gnumake python3 nasm xorriso mtools gptfdisk git qemu bear ];
+    packages = with pkgs; [ gcc binutils gnumake python3 nasm xorriso mtools gptfdisk git qemu bear c3c ];
 }
