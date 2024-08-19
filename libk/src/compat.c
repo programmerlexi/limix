5316ff4a986e09a0b2426b0e94775a3d7fa4b4b7
programmerlexi <60185691+truemaker@users.noreply.github.com>
2024-08-19 16:03:35 +0200

    Add compatibility stuff

---
+++ libk/src/compat.c
@@ -0,0 +1,7 @@
+#include "libk/utils/memory/memory.h"
+#include "string.h"
+
+void *memcpy(void *a, const void *b, size_t c) { return kmemcpy(a, b, c); }
+void *memset(void *a, int b, size_t c) { return kmemset(a, b, c); }
+int memcmp(const void *a, const void *b, size_t c) { return kmemcmp(a, b, c); }
+void __stack_chk_fail() {}
