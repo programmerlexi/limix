1973d937be4998b7152c2131b9406bcf8ce2cdcf
programmerlexi <60185691+truemaker@users.noreply.github.com>
2024-08-19 16:03:09 +0200

    Block the global scheduler on kernel panic

--- kernel/src/panic.c
+++ kernel/src/panic.c
@@ -2,6 +2,7 @@
 #include "kernel/gfx/vt/vt.h"
 #include "kernel/io/serial/serial.h"
 #include "kernel/kernel.h"
+#include "kernel/task/sched/global.h"
 
 __attribute__((noreturn)) void hcf(void) {
   asm("cli");
@@ -22,5 +23,7 @@ __attribute__((noreturn)) void kernel_panic() {
   serial_writes("An error has occured and the system will be halted.\n\r");
   if (vt_is_available())
     log(LOGLEVEL_FATAL, "An error has occured and the system will be halted.");
+  asm("cli");
+  sched_glob_aquire();
   hcf();
 }
