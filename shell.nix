7fdbaccf7eb1bb5eb0bfd6561113414630198a6f
programmerlexi <60185691+truemaker@users.noreply.github.com>
2024-10-20 09:54:27 +0200

    bear is working again

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
 { pkgs ? import <nixpkgs> {}, limine-bootloader }:
   pkgs.pkgsCross.x86_64-embedded.mkShell {
     name = "limix";
-    packages = with pkgs; [ wget gcc binutils gnumake python3 nasm xorriso mtools gptfdisk git qemu meson ninja gdb ];
+    packages = with pkgs; [ wget gcc binutils gnumake python3 nasm xorriso mtools gptfdisk git qemu meson ninja gdb bear ];
     LIMINE = "${limine-bootloader}";
 }
